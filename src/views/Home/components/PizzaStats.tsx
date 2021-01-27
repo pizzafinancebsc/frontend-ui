af72677efe09f878f545a63d01e5d0ca41136d67
JKRddy <imjaireddy@gmail.com>
2021-01-28 00:21:24 +0530

    ui upgrade v0.1.2

--- src/views/Home/components/PizzaStats.tsx
+++ src/views/Home/components/PizzaStats.tsx
@@ -42,7 +42,7 @@ const PizzaStats = () => {
         </Row>
         <Row>
           <Text fontSize="14px">{TranslateString(540, 'New PIZZA/block')}</Text>
-          <CardValue fontSize="14px" decimals={0} value={25} />
+          <CardValue fontSize="14px" decimals={0} value={1} />
         </Row>
       </CardBody>
     </StyledPizzaStats>
