32ae45ab254c92e33f0094622d84377c8ed80717
SolProc <info@cheeseswap.app>
2021-01-31 10:08:14 +0600

    add_chs

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -18,6 +18,23 @@ const pools: PoolConfig[] = [
     isFinished: false,
     tokenDecimals: 18,
   },
+  {
+   sousId: 1,
+   tokenName: 'CHS',
+   stakingTokenName: QuoteToken.PIZZA,
+   stakingTokenAddress: '0x2cc26dd730F548dc4ac291ae7D84a0C96980d2cB',    //
+   contractAddress: {
+     97: '',
+     56: '0xc2053223971ce57Cb1B68E078Da7dfd8c4541bAa',
+   },
+   poolCategory: PoolCategory.CORE,
+   projectLink: 'https://cheeseswap.app/',
+   harvest: true,
+   tokenPerBlock: '0.25',
+   sortOrder: 2,
+   isFinished: false,
+   tokenDecimals: 18,
+ },
 ]
 
 export default pools
