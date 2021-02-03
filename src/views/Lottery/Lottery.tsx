9d2bdcbecf245e00bf69209a2da0983d33d52b86
SolProc <info@cheeseswap.app>
2021-02-03 11:08:18 +0600

    lottery_api_rebuild

--- src/views/Lottery/Lottery.tsx
+++ src/views/Lottery/Lottery.tsx
@@ -31,7 +31,7 @@ const Lottery: React.FC = () => {
   const [mostRecentLotteryNumber, setMostRecentLotteryNumber] = useState(1)
 
   useEffect(() => {
-    fetch(`https://api.pizzafinance.app/api/lotteryHistory`)
+    fetch(`https://pizza-lottery-api.vercel.app/api/lotteryHistory`)
       .then((response) => response.json())
       .then((data) => setHistoryData(data))
       .catch(() => {
